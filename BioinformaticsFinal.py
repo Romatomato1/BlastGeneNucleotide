ab8d76facd317279bf64aba2c8bc3d1f6b3272b9
Roman Gofman <roman.p.gofman@gmail.com>
2023-06-06 14:01:27 -0700

    Add files via upload

--- BioinformaticsFinal.py
+++ BioinformaticsFinal.py
@@ -16,6 +16,45 @@ def read_file_contents(file_path):
         print(f"Error: Unable to read file {file_path}")
         return ""
 
+
+def calculate_differences(seq1, seq2):
+    """
+    Calculate the differences in dinucleotide pairs between two sequences.
+    """
+    differences = 0
+    for i in range(len(seq1) - 1):
+        pair1 = seq1[i:i + 2]
+        pair2 = seq2[i:i + 2]
+        if pair1 != pair2:
+            differences += 1
+    return differences
+
+
+def plot_dinucleotide_counts(seq, title):
+    """
+    Plot the counts of dinucleotide pairs in a sequence.
+    """
+    dinucleotides = [seq[i:i + 2] for i in range(len(seq) - 1)]
+    counts = [dinucleotides.count(pair) for pair in set(dinucleotides)]
+
+    plt.figure(figsize=(8, 6))
+    sns.barplot(x=list(set(dinucleotides)), y=counts)
+    plt.xlabel("Dinucleotide Pairs")
+    plt.ylabel("Count")
+    plt.title(title)
+    plt.xticks(rotation=45)
+    plt.show()
+
+
+def run_blast(sequence):
+    """
+    Perform a BLAST search using the given sequence.
+    """
+    result_handle = NCBIWWW.qblast("blastn", "nt", sequence)
+    blast_records = NCBIXML.parse(result_handle)
+    return blast_records
+
+
 fasta_file1 = r"C:\Users\roman\PycharmProjects\pythonProject3\NBPF10.fasta"
 fasta_file2 = r"C:\Users\roman\PycharmProjects\pythonProject3\NBPF11.fasta"
 
@@ -23,92 +62,24 @@ fasta_file2 = r"C:\Users\roman\PycharmProjects\pythonProject3\NBPF11.fasta"
 gene1_seq = read_file_contents(fasta_file1)
 gene2_seq = read_file_contents(fasta_file2)
 
-# Calculate the differences in nucleotides between the sequences
-differences = sum(a != b for a, b in zip(gene1_seq, gene2_seq))
+# Calculate the differences in dinucleotide pairs between the sequences
+differences = calculate_differences(gene1_seq, gene2_seq)
 
 # Display the number of differences
 print("Number of differences without BLAST:", differences)
 
-# Create a bar plot to visualize the differences in nucleotides without BLAST
-nucleotides = ['A', 'T', 'C', 'G']
-gene1_counts = [gene1_seq.count(nucleotide) for nucleotide in nucleotides]
-gene2_counts = [gene2_seq.count(nucleotide) for nucleotide in nucleotides]
+# Create two separate graphs for comparison
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-sns.barplot(x=nucleotides, y=gene1_counts, ax=ax1)
-ax1.set_xlabel("Nucleotides")
-ax1.set_ylabel("Count")
-ax1.set_title("NBPF10 Nucleotide Counts (without BLAST)")
+# Graph 1: NBPF10 Dinucleotide Counts
+plot_dinucleotide_counts(gene1_seq, "NBPF10 Dinucleotide Counts")
 
-sns.barplot(x=nucleotides, y=gene2_counts, ax=ax2)
-ax2.set_xlabel("Nucleotides")
-ax2.set_ylabel("Count")
-ax2.set_title("NBPF11 Nucleotide Counts (without BLAST)")
+# Graph 2: NBPF11 Dinucleotide Counts
+plot_dinucleotide_counts(gene2_seq, "NBPF11 Dinucleotide Counts")
 
-plt.tight_layout()
-plt.show()
-
-# Perform BLAST comparison for gene 1
-result_handle_gene1 = NCBIWWW.qblast("blastn", "nt", gene1_seq)
-
-# Parse the BLAST result for gene 1
-blast_record_gene1 = NCBIXML.read(result_handle_gene1)
-
-# Get the alignment results for gene 1
-alignments_gene1 = blast_record_gene1.alignments
-
-# Perform BLAST comparison for gene 2
-result_handle_gene2 = NCBIWWW.qblast("blastn", "nt", gene2_seq)
-
-# Parse the BLAST result for gene 2
-blast_record_gene2 = NCBIXML.read(result_handle_gene2)
-
-# Get the alignment results for gene 2
-alignments_gene2 = blast_record_gene2.alignments
-
-# Create a table to store the alignment results
-table_data = []
-
-# Display the alignment results for gene 1
-for alignment_gene1 in alignments_gene1:
-    alignment_info = []
-    alignment_info.append(alignment_gene1.title)
-    alignment_info.append(alignment_gene1.length)
-    alignment_info.append(alignment_gene1.hsps[0].expect)
-    table_data.append(alignment_info)
-
-# Display the alignment results for gene 2
-for alignment_gene2 in alignments_gene2:
-    alignment_info = []
-    alignment_info.append(alignment_gene2.title)
-    alignment_info.append(alignment_gene2.length)
-    alignment_info.append(alignment_gene2.hsps[0].expect)
-    table_data.append(alignment_info)
-
-# Print the table of alignment results
-headers = ["Title", "Length", "Expect"]
-print(tabulate(table_data, headers=headers))
-
-# Calculate the differences in nucleotides between the sequences
-differences = sum(a != b for a, b in zip(gene1_seq, gene2_seq))
-
-# Display the number of differences
-print("Number of differences with BLAST:", differences)
-
-# Create a bar plot to visualize the differences in nucleotides with BLAST
-gene1_counts = [gene1_seq.count(nucleotide) for nucleotide in nucleotides]
-gene2_counts = [gene2_seq.count(nucleotide) for nucleotide in nucleotides]
-
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-sns.barplot(x=nucleotides, y=gene1_counts, ax=ax1)
-ax1.set_xlabel("Nucleotides")
-ax1.set_ylabel("Count")
-ax1.set_title("NBPF10 Nucleotide Counts (with BLAST)")
-
-sns.barplot(x=nucleotides, y=gene2_counts, ax=ax2)
-ax2.set_xlabel("Nucleotides")
-ax2.set_ylabel("Count")
-ax2.set_title("NBPF11 Nucleotide Counts (with BLAST)")
-
-plt.tight_layout()
-plt.show()
+# Perform BLAST search for gene1_seq
+blast_results = run_blast(gene1_seq)
+for blast_record in blast_results:
+    # Process the BLAST results as needed
+    # Example: Print the alignment title and the first HSP (High-scoring Segment Pair)
+    print("Alignment title:", blast_record.alignments[0].title)
+    print("HSP bit score:", blast_record.alignments[0].hsps[0].bits)
